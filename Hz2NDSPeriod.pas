6d93d46bd9f4b4dc53287fe27de3a635bf663227
leo-the-leon <71839579+leo-the-leon@users.noreply.github.com>
2021-06-17 00:46:08 +0300

    Create Hz2NDSPeriod.pas

---
+++ Hz2NDSPeriod.pas
@@ -0,0 +1,18 @@
+{Converts Hz to Nintendo DS SOUNDxTMR timer values
+ by Leo the Leon/GTFrick}
+
+program HzToNDSPeriodConverter;
+
+const
+	NDSFreq=-16756991;
+	{this is assuming a 33.513982MHz bus clock, as used in
+	 the GBATEK document. ALL DS UNITS VARY IN CLOCK, so a
+	 fully accurate converter is not possible!}
+var
+	HzFreq:UInt32;
+begin
+	writeln('Enter frequency in hz: ');
+	readln (HzFreq);
+	writeln('Calculated NDS period: ', NDSFreq div HzFreq);
+	readln;
+end.
